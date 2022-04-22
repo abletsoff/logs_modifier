dbab43847f6729db5d3e8f3cc54dbde8bcbcabb3
Andrew Abletsov <abletsoff@gmail.com>
2022-04-22 11:14:19 +0300

    Update logs_modifier.py

--- logs_modifier.py
+++ logs_modifier.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python3
+
+import os
 import re
 import sys
 import argparse
 
 #=== TODO: Add Salted hash regular expressions
+
+# =============== Functions ===============
 
 def substitution(data, regex_str, name, excl_regex_str, detailed=False):
     
@@ -39,64 +44,18 @@ def arg_parser():
     parser.add_argument('-r', '--regex', action='store', help="user defined regex to modify")
     parser.add_argument('-P', '--pass_regex', action='store', help="user defined regex to not modify")
     parser.add_argument('-d', '--detail', action='store_true', help="display information about modified values")
-    parser.add_argument('files', metavar='FILE', type=str, nargs='+', help='file for modification')
+    parser.add_argument('-R', '--remove', action='store_true', help="remove original file")
+    parser.add_argument('path', metavar='PATH', type=str, nargs='+', help='path for modification procedures')
 
     return parser.parse_args()
     
-
-# ============ Regular Expressions ====================
-
-regex_MAC = (   r'(([0-9a-fA-F]{2}[:.]){5}'
-                r'[0-9a-fA-F]{2})|'
-                r'(([0-9a-fA-F]{4}[:.]){2}'
-                r'[0-9a-fA-F]{4})')
-
-#Exclude Broadcast MAC address from substitution
-regex_MAC_excl = (  r'(([fF]{2}[:.]){5}'
-                    r'[fF]{2})|'
-                    r'(([fF]{4}[:.]){2}'
-                    r'[fF]{4})')
-
-
-regex_IPv4 = (  r'([1-2]?\d{1,2}\.){3}'
-                r'[1-2]?\d{1,2}')
-
-#Exclude Loopback, Broadcast addresses and Mostly masks
-regex_IPv4_excl = ( r'(127\.0\.0\.1)|'
-                    r'(255\.([1-2]?\d{1,2}\.){2}'
-                    r'[1-2]?\d{1,2})')
-
-# This is monster "https://ihateregex.io/expr/ipv6/"
-regex_IPv6 = r'''(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'''
-
-#Exclude Loopback
-regex_IPv6_excl = r'::1'
-
-regex_Salted_Hashes =  r'\$2[abyx]\$\d\d\$.{53}' #bcrypt 
-
-#====================================================
-
-args = arg_parser()
-
-for file_name in args.files:
-  
-    try:
-        f = open(file_name)
-        data = f.read()
-        f.close()
-
-    except FileNotFoundError as excp:
-        print(excp)
-        exit()
-
-    except IsADirectoryError as excp:
-        print(excp)
-        exit()
-
-    except PermissionError as excp:
-        print(excp)
-        exit()
-    
+def modify_file(file_name, args):
+    f = open(file_name)
+    data = f.read()
+    f.close()
+   
+    if args.remove == True:
+        os.remove(file_name)
     if args.pass_regex == None:
         args.pass_regex = r'$a'
     if args.detail == True:
@@ -115,7 +74,62 @@ for file_name in args.files:
         print()
     if args.print == True:
         print(data)
-
+    
     f = open(file_name + '.modified', "w")
     f.write(data)
     f.close
+
+# =============== Regular Expressions ===============
+
+regex_MAC = (   r'(([0-9a-fA-F]{2}[:.]){5}'
+                r'[0-9a-fA-F]{2})|'
+                r'(([0-9a-fA-F]{4}[:.]){2}'
+                r'[0-9a-fA-F]{4})')
+
+# Exclude Broadcast MAC address from substitution
+regex_MAC_excl = (  r'(([fF]{2}[:.]){5}'
+                    r'[fF]{2})|'
+                    r'(([fF]{4}[:.]){2}'
+                    r'[fF]{4})')
+
+
+regex_IPv4 = (  r'([1-2]?\d{1,2}\.){3}'
+                r'[1-2]?\d{1,2}')
+
+# Exclude Loopback, Broadcast addresses and mostly masks
+regex_IPv4_excl = ( r'(127\.0\.0\.1)|'
+                    r'(255\.([1-2]?\d{1,2}\.){2}'
+                    r'[1-2]?\d{1,2})')
+
+# This is monster "https://ihateregex.io/expr/ipv6/"
+regex_IPv6 = r'''(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'''
+
+# Exclude Loopback
+regex_IPv6_excl = r'::1'
+
+# Salted hashes
+regex_Salted_Hashes =  r'\$2[abyx]\$\d\d\$.{53}' #bcrypt 
+
+# =============== Program Logic ===============
+
+args = arg_parser()
+
+for path in args.path:
+    try:
+        if os.path.isfile(path):
+            modify_file(path, args) 
+        elif os.path.isdir(path):
+            for root,d_names,f_names in os.walk(path):
+                for f_name in f_names:
+                    modify_file(f"{root}/{f_name}", args)
+        else:
+            raise FileNotFoundError(f"[Errno 2] No such file or directory: '{path}'")
+
+    except FileNotFoundError as excp:
+        print(excp)
+    
+    except PermissionError as excp:
+        print(excp)
+    
+    except UnicodeDecodeError as excp:
+        print(f'UnicodeDecodeError: {root}/{f_name}')
